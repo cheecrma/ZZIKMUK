8f1bce51e31a13c26d38530b6445a87d39fdb55b
Suhwan Im <suwhan1992@gmail.com>
2022-09-14 17:50:27 +0900

    [ISH] link mysql

--- backend/zzikmuk/settings.py
+++ backend/zzikmuk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'zzikmuk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
