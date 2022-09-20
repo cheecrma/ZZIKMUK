2625b7fc711ef95212eb8139c119bc06bed2b95e
surin kwon <tnfls7070@naver.com>
2022-09-20 13:49:09 +0900

    [KSR] update imageContainer component styled components 반영해 수정했고 그림자 효과 추가했습니다.

--- frontend_mobile/src/components/atom/ImageContainer.js
+++ frontend_mobile/src/components/atom/ImageContainer.js
@@ -1,26 +1,72 @@
-import { StyleSheet, Text, View, Image, Platform } from "react-native";
+import { Image, View } from "react-native";
+import styled from 'styled-components/native'
+
+const StyledImageContainer = styled(View)(({purpose}) => `
+  width: ${ getWidth(purpose) };
+  height: ${ getHeight(purpose) };
+  border-radius: 10px;
+  elevation: ${ getElevation(purpose) };
+  shadow-color: ${ getShadowColor(purpose) };
+`)
+
+function getWidth(purpose) {
+  switch (purpose) {
+    case "card":
+      return "50%"
+    case "recipe":
+      return "85%"
+    default:
+      return "30%"
+  }
+}
+
+function getHeight(purpose) {
+  switch (purpose) {
+    case "card":
+      return "15%"
+    case "recipe":
+      return "25%"
+    default:
+      return "30%"
+  }
+}
+
+function getElevation(purpose) {
+  switch (purpose) {
+    case "card":
+      return "0px"
+    default:
+      return "8px"
+  }
+}
+
+function getShadowColor(purpose) {
+  switch (purpose) {
+    case "card":
+      return "white"
+    default:
+      return "black"
+  }
+}
+
 
 export default function imageContainer({ purpose , url}) {
 
   return (
-    <Image 
-      source={{uri: url}} 
+    <StyledImageContainer 
+      purpose={purpose}
       style={{
-        width: purpose === 'card' ? '50%' :
-              purpose === 'recipe' ? '85%' : '30%',
-        height: purpose === 'card' ? '15%' :
-              purpose === 'recipe' ? '25%' : '30%',
-        ...Platform.select({
-          android: {elevation: 50},
-        }),
-        borderRadius: 10,
-        marginBottom: 10,
+        marginBottom: 30
       }}
-    />
+    >
+      <Image
+        source={{uri: url}}
+        style={{
+            width: '100%',
+            height: '100%',
+            borderRadius: 5
+          }} 
+        />
+    </StyledImageContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  imageContainer: {
-  },
-});
