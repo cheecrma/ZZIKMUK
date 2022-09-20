ee1b1f097f63ac0064263c2ca1e56fffeb59f26d
surin kwon <tnfls7070@naver.com>
2022-09-20 22:26:10 +0900

    [KSR] create recipeImage component 레시피 상세 화면에 쓰일 이미지 컴포넌트 만들었습니다.

---
+++ frontend_mobile/src/components/organism/RecipeImage.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import ExplainIcon from '../atom/ExplainIcon'
+
+export default function RecipeImage({ thumbnail, difficulty, amount, time }) {
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        style={styles.image}
+        source={{uri: thumbnail}}
+        imageStyle={{borderRadius: 10}}
+      >
+        <View style={styles.icons}>
+          <ExplainIcon type="difficulty" degree={difficulty} />
+          <ExplainIcon type="amount" iconText={amount} />
+          <ExplainIcon type="time" iconText={time} />
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 340, 
+    height: 200, 
+    borderRadius: 10, 
+    elevation: 10, 
+    backgroundColor: 'white', 
+    shadowColor: 'black'
+  },
+  image: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    width: 150,
+  }
+})
