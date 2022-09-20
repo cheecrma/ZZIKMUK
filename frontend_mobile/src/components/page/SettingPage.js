55d3b22d03b4319da8ba0086f8064a966b7d1ad3
cheecrma <cheecrma@gmail.com>
2022-09-20 17:04:07 +0900

    [KSL] update navbar js

--- frontend_mobile/src/components/page/SettingPage.js
+++ frontend_mobile/src/components/page/SettingPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function page() {
+export default function SettingPage() {
   return (
     <View style={styles.container}>
-      <Text>page</Text>
+      <Text>Setting</Text>
     </View>
   );
 }
