4d7129ced2f81ad63e64a04a6fff00a548e17288
surin kwon <tnfls7070@naver.com>
2022-09-23 15:23:18 +0900

    [KSR] fix RecipePage, modify RecipeStepPage 단계별 레시피 페이지에 수정해야 할 사항을 레시피 페이지에 수정해서 이를 고쳤습니다.

--- frontend_mobile/src/components/page/RecipePage.js
+++ frontend_mobile/src/components/page/RecipePage.js
@@ -1,27 +1,17 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableWithoutFeedback, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import RecipePagination from "../organism/RecipePagination"
-import Button from "../atom/Button"
+import RecipeImage from "../organism/RecipeImage";
+import IngredientList from "../organism/IngredientDetail"
+import RecipeIngredientPageBar from "../organism/RecipeIngredientPageBar";
+import RecipeDetail from "../organism/RecipeDetail";
+import Button from "../atom/Button";
 
+export default function RecipePage({ food }) {
+  const [index, setIndex] = useState(0)
 
-export default function RecipeStepPage({ food }) {
-  const [step, setStep] = useState(0)
-  const [isPlayed, setIsPlayed] = useState(true)
-  const [isMuted, setIsMuted] = useState(false)
-
-  function changeStep(index) {
-    setStep(index)
-  }
-
-  function playPauseToggle() {
-    setIsPlayed(!isPlayed)
-  }
-
-  function muteUnmuteToggle() {
-    setIsMuted(!isMuted)
+  function changeIndex(value) {
+    setIndex(value)
   }
 
   return (
@@ -34,68 +24,28 @@ export default function RecipeStepPage({ food }) {
         <View></View>
       </View>
       <View style={styles.content}>
-      <View style={styles.imageContainer}>
-        <ImageBackground 
-          style={styles.image}
-          source={{uri: food.thumbnail}}
-          imageStyle={{borderRadius: 10}}
-        >
-        </ImageBackground>
-      </View>
-        <View style={styles.step}>
-          <Text style={styles.stepText}>{food.recipe[step]}</Text>
-        </View>
-        <View style={styles.soundBtnContainer}>
-          <TouchableWithoutFeedback onPress={playPauseToggle}>
-            <View style={styles.soundBtn}>
-              {
-                isPlayed ? 
-                <AntDesign name="caretright" size={20} color="white" /> :
-                <Entypo name="controller-paus" size={20} color="white" />
-              }
-            </View>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={muteUnmuteToggle}>
-            <View style={styles.soundBtn}>
-              {
-                !isMuted ? 
-                <Feather name="volume-2" size={20} color="white" /> :
-                <Feather name="volume-x" size={20} color="white" />
-              }
-            </View>
-          </TouchableWithoutFeedback>
-        </View>
-        <View style={styles.explain}>
-          <Text>오케이 구글~ 레시피 넘겨줘</Text>
-        </View>
-        <RecipePagination totalSteps={food.recipe} checkedIndex={step} check={changeStep} />
-        <View style={styles.stepBtn}>
-          {
-            step > 0 ?
-            <Button 
-              variant="white" 
-              color="black" 
-              children="이전 단계"
-              size="small"
-            /> :
-            null
-          }
-          {
-            step === food.recipe.length - 1 ?
-            <Button 
-              variant="MainColor" 
-              color="white" 
-              children="요리 끝"
-              size="small"
-            /> :
-            <Button 
-              variant="MainColor" 
-              color="white" 
-              children="다음 단계"
-              size="small"
-            />
-          }
-        </View>
+        <RecipeImage 
+          thumbnail={food.thumbnail} 
+          difficulty={food.difficulty}
+          amount={food.amount}
+          time={food.time}
+        />
+        <RecipeIngredientPageBar
+          checkRecipe={changeIndex}
+          checkIngredient={changeIndex}
+          index={index}
+        />
+        {
+          index === 0 ?
+          <IngredientList ingredients={food.ingredients} /> :
+          <RecipeDetail recipe={food.recipe} />
+        }
+        <Button
+          children="요리 시작"
+          color="white"
+          variant="MainColor"
+          size="medium" 
+        />
       </View>
     </View>
   );
@@ -118,60 +68,7 @@ const styles = StyleSheet.create({
     fontWeight: "800",
   },
   content: {
-    flex: 24,
+    flex: 18,
     alignItems: "center",
-    justifyContent: "space-evenly"
-  },
-  step: {
-    backgroundColor: "white",
-    width: "90%",
-    height: 160,
-    borderRadius: 15,
-    justifyContent: "center",
-    padding: 5,
-  },
-  stepText: {
-    fontSize: 20,
-  },
-  soundBtnContainer: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "80%",
-  },
-  soundBtn: {
-    backgroundColor: "#FF8B34",
-    width: 40,
-    height: 40,
-    borderRadius: 40,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  explain: {
-    backgroundColor: "#FFE48E",
-    width: "90%",
-    height: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 5,
-  },
-  stepBtn: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-evenly",
-  },
-  imageContainer: {
-    width: 300, 
-    height: 160, 
-    borderRadius: 10, 
-    elevation: 10, 
-    backgroundColor: 'white', 
-    shadowColor: 'black'
-  },
-  image: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
   },
 });
