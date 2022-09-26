875b9b334458c117bf6b1a27b3ef7aad8513a8dc
Suhwan Im <suwhan1992@gmail.com>
2022-09-26 15:44:46 +0900

    [ISH] test rest api

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('ingredientall/<int:pk>/', views.IngredientInfo.as_view()),
+    path('<int:id>/', views.RecipeInfo.as_view()),
+    path('detail/<int:rid>/<int:rstep>/', views.RecipeDetailInfo.as_view()),
+    path('ingredient/<int:no>/', views.RecipeIngredientInfo.as_view()),
 ]
