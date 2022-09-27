0d316b2ffda2b30d110fc68b5d35de888353d828
surin kwon <tnfls7070@naver.com>
2022-09-27 17:10:30 +0900

    [KSR] create MicroPage 간단한 마이크로 페이지 레이아웃 생성했습니다.

---
+++ frontend_web/src/components/MicroPage.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="background">
+    <div class="phone">
+      <button class="camera-btn"></button>
+    </div>
+    <swiper
+      :pagination="true"
+      :navigation="true"
+      :modules="modules"
+      :swiper-per-view="auto"
+      :space-between="50"
+      class="swiper-wrapper"
+      :speed="1000"
+    >
+      <swiper-slide class="swiper-slide">
+        <div class="inner">
+          <p>찍먹마트</p>
+          <p><span>소세지 </span><span>2000원</span></p>
+          <p><span>옥수수 </span><span>1200원</span></p>
+          <p><span>치즈 </span><span>6000원</span></p>
+        </div>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide">
+        <div class="inner"></div>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide">
+        <div class="inner"></div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default {
+  name: "MicroPage",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const swiperOptions = {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      slidesPerView: "auto",
+      spaceBetween: 50,
+      loop: true,
+      centeredSlides: true,
+      speed: 1000,
+      effect: "coverflow",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: -100,
+        depth: 400,
+        modifier: 1,
+        slideShadows: false,
+      },
+    };
+
+    return {
+      swiperOptions,
+      modules: [Navigation, Pagination, Scrollbar, A11y],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  box-sizing: border-box;
+  background: linear-gradient(36deg, #ffc061, #ffecae);
+}
+
+.phone {
+  background-color: black;
+  width: 300px;
+  height: 500px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 100px;
+  left: 618px;
+}
+
+.camera-btn {
+  background-color: white;
+  width: 40px;
+  height: 40px;
+  align-self: flex-end;
+  border-radius: 50%;
+  margin-bottom: 24px;
+}
+
+.swiper-wrapper {
+  width: 500px;
+  height: 100%;
+}
+
+.swiper-wrapper .swiper-slide {
+  width: 500px;
+  height: 500px;
+  position: relative;
+  top: 50%;
+  margin-top: -230px;
+  display: flex;
+  justify-content: center;
+}
+
+.swiper-wrapper .swiper-slide .inner {
+  width: 252px;
+  height: 380px;
+  background: #fff;
+  padding: 30px;
+  box-sizing: border-box;
+  opacity: 0.4;
+  transition: 1s;
+}
+
+.swiper-wrapper .swiper-slide-active .inner {
+  opacity: 1;
+  box-shadow: 0px 0px 50px rgba(255, 255, 255, 0.8);
+}
+
+.swiper-wrapper .swiper-slide .inner::before,
+.swiper-wrapper .swiper-slide .inner::after {
+  content: "";
+  display: block;
+  width: 252px;
+  height: 380px;
+  position: absolute;
+  top: 0px;
+  left: 130px;
+  opacity: 0;
+  background: #fff;
+}
+
+.swiper-wrapper .swiper-slide-prev .inner,
+.swiper-wrapper .swiper-slide-next .inner {
+  opacity: 0.7;
+}
+
+@keyframes ani {
+  0% {
+    opacity: 0;
+    transform: scale(1);
+  }
+  1% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0;
+    transform: scale(1.2);
+  }
+}
+
+.swiper-wrapper .swiper-slide-active .inner::before {
+  animation: ani 1s ease 1;
+}
+.swiper-wrapper .swiper-slide-active .inner::after {
+  animation: ani 1s ease 0.3s 1;
+}
+</style>
