2aea6f82947658d9775bf653e7baf69e916437a3
Suhwan Im <suwhan1992@gmail.com>
2022-09-28 09:22:39 +0900

    [ISH] update requirement.txt

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,10 @@
+from http.client import responses
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from django.http import Http404
 
 from .serializers import IngredientSerializer, RecipeSerializer, RecipeDetailSerializer, RecipeIngredientSerializer
