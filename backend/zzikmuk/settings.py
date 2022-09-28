a681e4af7bdbe67c8b40177c48c47b7324d09a45
Suhwan Im <suwhan1992@gmail.com>
2022-09-28 11:25:23 +0900

    [ISH] update views.py

--- backend/zzikmuk/settings.py
+++ backend/zzikmuk/settings.py
@@ -130,7 +130,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+    'https://j7a102.p.ssafy.io',
+]
 
 # CORS_ORIGIN_ALLOW_ALL = True
 
