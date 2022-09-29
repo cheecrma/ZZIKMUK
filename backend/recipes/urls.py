c13e98a8c275bdcf9abb594c23349fb964db3444
Suhwan Im <suwhan1992@gmail.com>
2022-09-29 14:01:40 +0900

    [ISH] update recipe step & complete api

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -6,13 +6,10 @@ app_name = 'recipes'
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    #path('ingredientall/', views.IngredientInfo.as_view()),
-    #path('<int:id>/', views.RecipeInfo.as_view()),
-    # path('detail/<int:rid>/<int:rstep>/', views.RecipeDetailInfo.as_view()),
-    #path('detail/', views.RecipeDetailInfo.as_view()),
-    #path('ingredient/', views.RecipeIngredientInfo.as_view()),
-    path('tips/<int:id>/', views.TipsInfo.as_view()),
-
     path('list/<int:id>/', views.RecipeList.as_view()),
     path('detail/', views.RecipeDetailList.as_view()),
+    path('step/', views.RecipeStepList.as_view()),
+    path('complete/', views.RecipeCompleteList.as_view()),
+    #path('popular/', views.RecipePopularList.as_view()),
+    path('tips/<int:id>/', views.TipsInfo.as_view()),
 ]
