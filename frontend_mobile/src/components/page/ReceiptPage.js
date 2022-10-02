201072290bf41c6204b5d9a112ae2525615e8c94
cheecrma <cheecrma@gmail.com>
2022-10-02 23:31:36 +0900

    [KSL] 영수증 재료 화면 보이게 하기

--- frontend_mobile/src/components/page/ReceiptPage.js
+++ frontend_mobile/src/components/page/ReceiptPage.js
@@ -3,12 +3,17 @@ import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import ReceiptCheck from "../organism/ReceiptCheck";
 import Button from "../atom/Button";
+import CameraReceipt from "./CameraReceipt";
 
-export default function ReceiptPage() {
+export default function ReceiptPage({ navigation, route }) {
+  // function goReceiptRecommendPage() {
+  //   navigation.navigate("Recommend", { });
+  // }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => navigation.pop()}>
           <AntDesign name="arrowleft" size={24} color="black" />
         </TouchableWithoutFeedback>
         <Text style={styles.title}>
@@ -17,11 +22,18 @@ export default function ReceiptPage() {
         <View></View>
       </View>
       <View style={styles.receipt}>
-        <ReceiptCheck />
+        <ReceiptCheck receipt={route.params.receipt} />
         <Text>원하는 재료를 추가해서 다양한 레시피를 제공 받아 보세요.</Text>
       </View>
       <View style={{ flex: 1 }}>
-        <Button color="white" variant="BoldColor" size="large">
+        <Button
+          color="white"
+          variant="BoldColor"
+          size="large"
+          // onPress={() => {
+          //   goReceiptRecommendPage();
+          // }}
+        >
           추천 레시피 확인하러 가기
         </Button>
       </View>
