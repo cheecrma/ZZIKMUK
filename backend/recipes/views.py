3f42aaa8bdabf28c5d7dfd292964fcd1e9b43b74
Suhwan Im <suwhan1992@gmail.com>
2022-10-02 22:16:14 +0900

    [ISH] create suggestion algorithm using cosine similarity

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,6 +7,11 @@ from rest_framework.response import Response
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from django.http import Http404
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
+from operator import itemgetter
+
 
 from .serializers import IngredientSerializer, RecipeSerializer, RecipeDetailSerializer, RecipeIngredientSerializer, TipsSerializer
 from .models import Ingredient, Recipe, RecipeDetail, RecipeIngredient, Tips
@@ -183,7 +188,7 @@ class IngredientSearch(APIView):
             else:
                 return("검색하신 '" + text + "'에 일치하는 재료가 없습니다.")
 
-        except Recipe.DoesNotExist:
+        except Ingredient.DoesNotExist:
             raise Http404
 
     @swagger_auto_schema(operation_id="재료 검색 기능", operation_description="String 입력으로 재료 검색", request_body=param)
@@ -203,20 +208,60 @@ class TipsInfo(APIView):
 
 # 레시피 추천 POST
 class RecipeSuggestion(APIView):
-    param = openapi.Schema(type=openapi.TYPE_OBJECT, required=['ingredients'],
+    param = openapi.Schema(type=openapi.TYPE_OBJECT, required=['user_ingredients'],
     properties={
-        'ingredients': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description="재료 리스트"),
-    }) 
-    def get_object(self, ingredients):
+        'user_ingredients': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description="재료 리스트"),
+    })
+
+    def get_object(self, user_ingredients):
         try:
-            print(ingredients) # 재료 리스트
+            # 코사인 유사도 함수
+            def cos_sim(a, b):
+                return dot(a, b)/(norm(a)*norm(b))
 
-            # return("입력한 재료는 '" + i_list + "'입니다.")
+            # 모든 재료 리스트와 특정 재료 리스트를 비교 후 벡터행렬 생성 함수
+            def make_matrix(feats, list_data):
+                freq_list = []
+                for feat in feats:
+                    freq = 0
+                    for word in list_data:
+                        if feat == word:
+                            freq += 1
+                    freq_list.append(freq)
+                return freq_list
+            
+            print(user_ingredients) # 사용자가 입력한 재료 리스트 ['string', 'string2', 'string3']
+            ingre_db = Ingredient.objects.all() # DB의 전체 재료 불러오기
+            ingre_db_list = []
+            for i_db in ingre_db:               # DB에 있는 모든 재료를 리스트로 변환
+                ingre_db_list.append(i_db.name)
+            feats = set(user_ingredients + ingre_db_list)    # 사용자가 입력한 재료 리스트와 DB 전체 리스트를 세트로 묶기
 
-        except Recipe.DoesNotExist:
+            cs_list = []        # 모든 레시피와의 코사인 유사도를 담을 cs_list 생성
+            recipe = Recipe.objects.all()
+            for i in range(len(recipe)):
+                ingredients = RecipeIngredient.objects.filter(recipe_id = recipe[i].id)
+                ing_list = []
+                for j in ingredients:
+                    ing_list.append(j.ingredient_id.name)
+                
+                ing_arr = np.array(make_matrix(feats, user_ingredients))     # 사용자 입력 재료
+                rec_arr = np.array(make_matrix(feats, ing_list))        # 해당 레시피 재료
+
+                cs = cos_sim(ing_arr, rec_arr)          # 코사인 유사도 값 구하기
+                cs_list.append([recipe[i].id, cs])      # cs_list에 [레시피 번호, 코사인 유사도 값] 누적하기
+            
+            sorted_list = sorted(cs_list, key=itemgetter(1), reverse=True)  # 전체 코사인 유사도를 내림차순으로 정렬
+            rlt_list = []       # 코사인 유사도가 높은순으로 레시피 번호를 담을 rlt_list 생성
+            for rlt in sorted_list[:20]:        # 코사인 유사도 순위대로 n개의 레시피 번호 담기 
+                rlt_list.append(rlt[0])
+            
+            return(rlt_list)
+
+        except Ingredient.DoesNotExist:
             raise Http404
 
     @swagger_auto_schema(operation_id="레시피 추천 기능", operation_description="재료 리스트 입력으로 최적의 레시피 추천", request_body=param)
     def post(self, request, format=None):
-        suggestion_rlt = self.get_object(request.data['ingredients'])
+        suggestion_rlt = self.get_object(request.data['user_ingredients'])
         return Response(suggestion_rlt)
